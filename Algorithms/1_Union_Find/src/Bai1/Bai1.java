9f7d46a589bed60103dbce71cad8c651d31d8d44
fushi <kaiki0201a@gmail.com>
2026-09-08 00:35:51 +0700

    Làm bài tập 1-2

--- Algorithms/1_Union_Find/src/Bai1/Bai1.java
+++ Algorithms/1_Union_Find/src/Bai1/Bai1.java
@@ -9,5 +9,9 @@ public void union(int p, int q) {
 - Vấn đề: không lưu lại giá trị của leader[p]
 -> khi leader[p] thay đổi thành leader[q]
 -> những phần tử khác thuộc tập cùng leader[p] sẽ không được union
-
+- Test case chạy sai:
+1. Khởi tạo mảng n = 3 gồm các phần tử (0,1,2)
+2. Mảng ban đầu: [0, 1, 2]
+3. Thực hiện union(1,0) -> Mảng sau union: [0, 0, 2]
+4. Thưc hiện union(0,2) -> Mảng sau union: [2, 0, 2] -> đỉnh 1 không được cập nhật
  */
---
+++ Algorithms/1_Union_Find/src/Weight_QuickUnion.java
@@ -0,0 +1,40 @@
+// Weight: co them thuoc tinh sz; khi gop se danh gia sz; noi cay be vao cay lon
+// Lưu ý: cài đặt weighted quick-union cho union(p,q) của chúng ta sẽ thay đổi parent[q] nếu hai cây có cùng kích thước.)
+
+public class Weight_QuickUnion{
+    private int[] id;
+    private int[] sz;
+
+    public Weight_QuickUnion(int N){
+        id = new int[N];
+        sz = new int[N];
+        for (int i = 0; i < N; i++){
+            id[i] = i;
+            sz[i] = 1;
+        }
+    }
+
+    private int root(int i){
+        while (i != id[i])
+            i = id[i];
+        return i;
+    }
+
+    public boolean connected(int p, int q){
+        return root(p) == root(q);
+    }
+
+    public void union(int p, int q){
+        int i = root(p);
+        int j = root(q);
+
+        if (i == j) return;
+        if (sz[i] >= sz[j]){
+            id[j] = i;
+            sz[i] += sz[j]
+        } else {
+            id[i] = j;
+            sz[j] += sz[i];
+        }
+    }
+}
